0b7127f1dc4c19cfcf52fb548a24a8135212e1a4
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2021-09-06 16:44:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { ProductDisplayPage } from "./Product-Page/product-display-page";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ProductDisplayPage />
+    </div>
+  );
+}
---
+++ src/productDB/productDB.js
@@ -0,0 +1,83 @@
+export const productDB = [
+  {
+    productName: "Stripped Men round neck...",
+    originalPrice: 272,
+    offeredPrice: 1399,
+    imgSrc:
+      "https://rukminim1.flixcart.com/image/880/1056/keykscw0-0/t-shirt/l/d/q/3xl-bmrgyrnful-z12-blive-original-imafvgzkyjghf7ba.jpeg?q=50",
+    size: "XL",
+    brand: "Urbano",
+    gender: "Men"
+  },
+  {
+    productName: "Printed Men Round Neck Dar...",
+    originalPrice: 2399,
+    offeredPrice: 1099,
+    imgSrc:
+      "https://rukminim1.flixcart.com/image/880/1056/kf5pzm80-0/t-shirt/2/v/v/xl-aopleaffull-drgreen-urbano-fashion-original-imafvzqqbfbfvprv.jpeg?q=50",
+    size: "L",
+    brand: "Urbano",
+    gender: "Men"
+  },
+
+  {
+    productName: "SGirls Regular Fit Solid Spr...",
+    originalPrice: 759,
+    offeredPrice: 1399,
+    imgSrc:
+      "https://rukminim1.flixcart.com/image/880/1056/kiew3gw0/shirt/w/a/j/11-12-years-cc4235a-plum-cutecumber-original-imafy7gzbkymqhdd.jpeg?q=50",
+    size: "S",
+    brand: "Cutecumber",
+    gender: "Girl"
+  },
+  {
+    productName: "Boys Slim Fit Solid Spread Col...",
+    originalPrice: 356,
+    offeredPrice: 1799,
+    imgSrc:
+      "https://rukminim1.flixcart.com/image/880/1056/kmf7ki80/shirt/7/q/w/13-14-years-solid-cotton-shirt-for-boys-cavenders-original-imagfbywzkwzhdhh.jpeg?q=50",
+    size: "S",
+    brand: "Cavenders",
+    gender: "Boy"
+  },
+  {
+    productName: "Boys regular fit color...",
+    originalPrice: 474,
+    offeredPrice: 2599,
+    imgSrc:
+      "https://rukminim1.flixcart.com/image/880/1056/klgx0280/shirt/b/b/m/13-14-years-twtblshirtfulboy-bsh4-tripr-original-imagyhetahvf5bqx.jpeg?q=50",
+    size: "L",
+    brand: "Cavenders",
+    gender: "Boy"
+  },
+  {
+    productName: "Girls Solid Mandarin Co...",
+    originalPrice: 429,
+    offeredPrice: 999,
+    imgSrc:
+      "https://rukminim1.flixcart.com/image/880/1056/kh6lg280-0/shirt/c/r/p/m-slim-fit-shirt-oriex-original-imafx94m2ktgzfev.jpeg?q=50",
+    size: "L",
+    brand: "Cutecumber",
+    gender: "Girl"
+  },
+  {
+    productName: "Women regular fit printed...",
+    originalPrice: 599,
+    offeredPrice: 1299,
+    imgSrc:
+      "https://rukminim1.flixcart.com/image/880/1056/kr83ukw0/shirt/4/b/p/s-grcssb-shitala-original-imag5fkuzfkffe3f.jpeg?q=50",
+    size: "XL",
+    brand: "Cutecumber",
+    gender: "Women"
+  },
+  {
+    productName: "Women regular fit printed...",
+    originalPrice: 1298,
+    offeredPrice: 2399,
+    imgSrc:
+      "https://rukminim1.flixcart.com/image/880/1056/ksc46fk0/shirt/g/l/g/l-white-vdp-enterprise-original-imag5xqjcgh349qa.jpeg?q=50",
+    size: "XL",
+    brand: "Cutecumber",
+    gender: "Women"
+  }
+];
